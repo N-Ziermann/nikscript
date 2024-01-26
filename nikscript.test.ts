e8c97d88658993aa7e1831ecc37320fecd0ce441
Niklas Ziermann <n-ziermann@protonmail.com>
2024-01-26 22:23:16 +0100

    Add typescript and jest

---
+++ nikscript.test.ts
@@ -0,0 +1,44 @@
+import { interpret } from './nikscript.ts';
+
+let logSpy: jest.SpyInstance | undefined = undefined;
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  logSpy = jest.spyOn(console, 'log');
+});
+
+it('should print a computed value', () => {
+  const code = 'print(3 + 8);';
+  interpret(code);
+  expect(logSpy?.mock.calls.map((args) => args[0])).toEqual([11]);
+});
+
+it('should do multiple statements one after the other', () => {
+  const code = 'print(1); print(2);';
+  interpret(code);
+  expect(logSpy?.mock.calls.map((args) => args[0])).toEqual([1, 2]);
+});
+
+it('should print each value in a for loop', () => {
+  const code = 'for(i=1<4){print(i);}';
+  interpret(code);
+  expect(logSpy?.mock.calls.map((args) => args[0])).toEqual([1, 2, 3]);
+});
+
+it('should print each fizzbuzz value from 1 to 100', () => {
+  const FizzBuzz =
+    'for(i=1<101){tmp = "";if(i%3==0){tmp = tmp + "Fizz";}if(i%5==0){tmp = tmp + "Buzz";}if(tmp==""){tmp=i;}print(tmp);}';
+  interpret(FizzBuzz);
+  expect(logSpy?.mock.calls.map((args) => args[0])).toEqual(fizzbuzz(100));
+});
+
+function fizzbuzz(maxValue: number) {
+  const result: (string | number)[] = [];
+  for (let i = 1; i <= maxValue; i++) {
+    if (i % 15 === 0) result.push('FizzBuzz');
+    else if (i % 3 === 0) result.push('Fizz');
+    else if (i % 5 === 0) result.push('Buzz');
+    else result.push(i);
+  }
+  return result;
+}
