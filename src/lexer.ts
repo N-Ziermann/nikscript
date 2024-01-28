47adcf8e2fa5361a04eb1ce76c1ade63a57c59c5
Niklas Ziermann <n-ziermann@protonmail.com>
2024-01-28 11:57:29 +0100

    Split up files

---
+++ src/lexer.ts
@@ -0,0 +1,96 @@
+export function lexer(code: string): Token[] {
+  let tokens: Token[] = [];
+  let index = -1;
+
+  let lastTokenized = ' '; // stores last accepted tokenvalue in case something (like negative numbers) depend on it
+
+  while (index < code.length - 1) {
+    index += 1;
+    let char = code[index];
+
+    if (char === ' ' || char === '\n') {
+      continue;
+    } else if (char === ';') {
+      // TODO: ["CHARACTER", char]
+      tokens.push([char, '']);
+    } else if (char === '#') {
+      //comments
+      while (char !== '\n') {
+        index += 1;
+        char = code[index];
+      }
+    } else if (
+      char.match(/[0-9]/) ||
+      (char === '-' &&
+        code[index + 1].match(/[0-9]/) &&
+        lastTokenized.match(/[\n (+-/*%=]/))
+    ) {
+      // "||" necessary to prevent mixups between x-1 and print(-1)
+      let numberAsString = char;
+      index += 1;
+      char = code[index];
+
+      while (char.match(/[0-9\.]/)) {
+        numberAsString += char;
+        index += 1;
+        char = code[index];
+      }
+      tokens.push(['number', numberAsString]);
+      index -= 1; //prevent loosing data
+      lastTokenized = code[index];
+    } else if (
+      char === '+' ||
+      char === '-' ||
+      char === '/' ||
+      char === '*' ||
+      char === '%'
+    ) {
+      // TODO: operator etc should be union and UPPERCASE!
+      tokens.push(['operator', char]);
+      lastTokenized = char;
+    } else if (char === '"') {
+      let stringContent = '';
+      index += 1;
+      char = code[index];
+
+      while (char !== '"') {
+        stringContent += char;
+        index += 1;
+        char = code[index];
+      }
+
+      tokens.push(['string', stringContent]);
+      lastTokenized = 'string';
+    } else if (char.match(/[\<\>\(\)\{\}\[\],=]/)) {
+      tokens.push([char as SpecialCharacter, '']);
+      lastTokenized = char;
+    }
+
+    if (char.match(/[a-zA-Z]/)) {
+      let term = char;
+      index += 1;
+      char = code[index];
+
+      while (char.match(/[a-zA-Z0-9_]/)) {
+        term += char;
+        index += 1;
+        char = code[index];
+      }
+
+      if (
+        term === 'if' ||
+        term === 'else' ||
+        term === 'for' ||
+        term === 'while'
+      ) {
+        tokens.push(['statement', term]);
+      } else {
+        tokens.push(['name', term]);
+      }
+      index -= 1; //prevent loosing data
+      lastTokenized = 'name';
+    }
+  }
+  tokens.push(['END', 'END']);
+  return tokens;
+}
